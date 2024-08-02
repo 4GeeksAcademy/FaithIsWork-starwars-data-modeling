64686c53ecf4ca79caf37cea7945f2936c9ae0ab
FaithIsWork11 <faithiswork@duck.com>
2024-08-02 21:04:35 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -7,23 +7,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='user')
+
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='planet', )
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+
+class People(Base):
+    __tablename__ = 'people'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey(User.id),primary_key=True)
+    name = Column(String(250), nullable=False)
+    favorites =  relationship('Favorite', backref='people')
+    
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=True)
+  
+
+
 
     def to_dict(self):
         return {}
